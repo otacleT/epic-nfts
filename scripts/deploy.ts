8274eb510e4a76488825ce4feb5e14ce9514f296
MIYABETaisei <mytspe46@gmail.com>
2022-09-29 14:54:59 +0900

    ✨ テストネットワークにデプロイ

---
+++ scripts/deploy.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+  console.log("Minted NFT #1");
+  txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+  console.log("Minted NFT #2");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();

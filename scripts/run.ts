353b9941cfc6a4d85930bf107c60ebf5b81517f2
MIYABETaisei <mytspe46@gmail.com>
2022-09-29 14:54:45 +0900

    ✨ コントラクトのテストコード更新

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 // run.js
 const main = async () => {
-  // コントラクトがコンパイルします
+  // コントラクトがコンパイルされる。
   // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
   const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
   // Hardhat がローカルの Ethereum ネットワークを作成します。
@@ -10,6 +10,10 @@ const main = async () => {
   // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+  txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
 };
 // エラー処理を行っています。
 const runMain = async () => {
@@ -21,3 +25,4 @@ const runMain = async () => {
     process.exit(1);
   }
 };
+runMain();
